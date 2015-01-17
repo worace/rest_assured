dab0944622c4dd913074cc11b250c63f5e6d0af6
worace <horace.d.williams@gmail.com>
2015-01-17 13:50:30 -0700

    add index view for shelters

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,7 @@
 class Shelter < ActiveRecord::Base
   has_many :availability_reports
+
+  def beds_available
+    availability_reports.last.number_of_beds
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root :to => "shelters#index"
+
+  resources :shelters, :only => [:show, :index]
+
   namespace :admin do
     root :to => "shelters#index"
     resources :shelters do
