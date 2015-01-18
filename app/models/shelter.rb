c38e4a74697a867ff2c19a93bba2ee668c75b92a
worace <horace.d.williams@gmail.com>
2015-01-18 11:02:55 -0700

    add method for selecting shelters that we need to check on

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,7 +1,12 @@
 class Shelter < ActiveRecord::Base
+  CHECKIN_INTERVAL = 10.minutes
   has_many :availability_reports
 
   def beds_available
     availability_reports.last.number_of_beds
+  end
+
+  def self.shelters_within_last_checkin_interval
+    Shelter.where("checkin_time::time >= ? AND checkin_time::time <= ?", CHECKIN_INTERVAL.ago, Time.now)
   end
 end
