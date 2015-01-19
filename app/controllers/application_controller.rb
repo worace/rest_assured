949e4c648b66787113e23eedab4fcf2f44c24c0b
worace <horace.d.williams@gmail.com>
2015-01-18 18:45:46 -0700

    add error logger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #rescue_from Exception do |exception|
-    #render :text => "###### #{exception.message}\n\n#{exception.backtrace.join("\n")}"
-  #end
+  rescue_from Exception do |exception|
+    Rails.logger.error("********* EXCEPTION RAISED: #{exceiption.message} *************")
+    Rails.logger.error(exception.backtrace.join("\n"))
+    raise exception
+  end
 end
