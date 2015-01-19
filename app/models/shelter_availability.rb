9982aaeeeffc9a1de27b621a79a4ff27e7420ea6
worace <horace.d.williams@gmail.com>
2015-01-18 17:06:29 -0700

    tweak message coming out of scheduled job

--- app/models/shelter_availability.rb
+++ app/models/shelter_availability.rb
@@ -5,7 +5,7 @@ class ShelterAvailability
     Rails.logger.info("****************** CHECKING ON THE CHECKINS ****************")
     Shelter.shelters_needing_checkin.each do |shelter|
       Rails.logger.info("*** CHECKING CHECKINS FOR SHELTER: #{shelter} ***")
-      client.messages.create(from: TWILIO_NUMBER, to: HORACE_NUMBER, body: "Automated Check In message from Rest Assured -- Please respond with bed availability for: #{shelter.name}")
+      client.messages.create(from: TWILIO_NUMBER, to: HORACE_NUMBER, body: "Automated Check In message from Rest Assured -- Please respond with 'update <number of beds>' to update bed availability for: #{shelter.name}. Or reply with 'options' for more info")
     end
   end
 end
