5709b8955e297ea00166db558959374c96d6b525
worace <horace.d.williams@gmail.com>
2015-01-18 18:11:26 -0700

    remove update to shelter checkin times in availability script

--- app/models/shelter_availability.rb
+++ app/models/shelter_availability.rb
@@ -1,6 +1,5 @@
 class ShelterAvailability
   def self.check_shelters
-    Shelter.all.each { |s| s.update_attribute(:checkin_time, 2.minutes.ago) }
     client = Twilio::REST::Client.new
     Rails.logger.info("****************** CHECKING ON THE CHECKINS ****************")
     Shelter.shelters_needing_checkin.each do |shelter|
